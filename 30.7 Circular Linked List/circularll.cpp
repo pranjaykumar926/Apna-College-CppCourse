1cbbad579450b7f8642e61712796115488e270f6
Pranjay kumar <110048711+pranjaykumar926@users.noreply.github.com>
2025-12-18 23:22:47 +0530

    Refactor circular linked list implementation

--- 30.7 Circular Linked List/circularll.cpp
+++ 30.7 Circular Linked List/circularll.cpp
@@ -1,114 +1,138 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-class node{
-
-    public:
+class node {
+public:
     int data;
     node* next;
 
-    node(int val){
-        data=val;
-        next=NULL;
+    node(int val) {
+        data = val;
+        next = NULL;
     }
 };
 
-void insertAtHead(node* &head, int val){
-    node* n=new node(val);
+/* Insert at Head */
+void insertAtHead(node* &head, int val) {
+    node* n = new node(val);
 
-    if(head==NULL){
-        n->next=n;
-        head=n;
+    if (head == NULL) {
+        n->next = n;
+        head = n;
         return;
     }
-    node* temp=head;
 
-    while(temp->next!=head){
-        temp=temp->next;
+    node* temp = head;
+    while (temp->next != head) {
+        temp = temp->next;
     }
-    temp->next=n;
-    n->next=head;
-    head=n;
+
+    temp->next = n;
+    n->next = head;
+    head = n;
 }
 
-void insertAtTail(node* &head, int val){
-
-    if(head==NULL){
-        insertAtHead(head,val);
+/* Insert at Tail */
+void insertAtTail(node* &head, int val) {
+    if (head == NULL) {
+        insertAtHead(head, val);
         return;
     }
 
     node* n = new node(val);
-    node* temp=head;
+    node* temp = head;
 
-    while(temp->next!=head){
-        temp=temp->next;
+    while (temp->next != head) {
+        temp = temp->next;
     }
 
-    temp->next=n;
-    n->next=head;
+    temp->next = n;
+    n->next = head;
 }
 
-void deleteAtHead(node* &head){
-    node* temp=head;
-    while(temp->next!=head){
-        temp=temp->next;
+/* Delete Head Node */
+void deleteAtHead(node* &head) {
+    if (head == NULL)
+        return;
+
+    // Only one node
+    if (head->next == head) {
+        delete head;
+        head = NULL;
+        return;
     }
 
-    node* todelete=head;
-    temp->next=head->next;
-    head=head->next;
+    node* temp = head;
+    while (temp->next != head) {
+        temp = temp->next;
+    }
 
+    node* todelete = head;
+    temp->next = head->next;
+    head = head->next;
     delete todelete;
 }
 
-void deletion(node* &head, int pos){
+/* Delete at Given Position */
+void deletion(node* &head, int pos) {
+    if (head == NULL || pos <= 0)
+        return;
 
-    if(pos==1){
+    if (pos == 1) {
         deleteAtHead(head);
         return;
     }
-      node* temp=head;
-      int count=1;
 
-      while(count!=pos-1){
-          temp=temp->next;
-          count++;
-      }
+    node* temp = head;
+    int count = 1;
 
-      node* todelete=temp->next;
-      temp->next=temp->next->next;
+    while (count < pos - 1 && temp->next != head) {
+        temp = temp->next;
+        count++;
+    }
 
-      delete todelete;
+    // Invalid position
+    if (temp->next == head)
+        return;
+
+    node* todelete = temp->next;
+    temp->next = temp->next->next;
+    delete todelete;
 }
 
-void display(node* head){
+/* Display List */
+void display(node* head) {
+    if (head == NULL) {
+        cout << "List is empty" << endl;
+        return;
+    }
 
+    node* temp = head;
+    do {
+        cout << temp->data << " ";
+        temp = temp->next;
+    } while (temp != head);
 
-    node* temp=head;
-    do
-    {
-        cout<<temp->data<<" ";
-        temp=temp->next;
-    } while (temp!=head);
-    cout<<endl;
-    
+    cout << endl;
 }
 
+int main() {
+    node* head = NULL;
 
-int main(){
+    insertAtTail(head, 1);
+    insertAtTail(head, 2);
+    insertAtTail(head, 3);
+    insertAtTail(head, 4);
+    display(head);
 
-    node* head=NULL;
-    insertAtTail(head,1);
-    insertAtTail(head,2);
-    insertAtTail(head,3);
-    insertAtTail(head,4);
+    insertAtHead(head, 5);
     display(head);
-    insertAtHead(head,5);
+
+    deletion(head, 1);
     display(head);
-    deletion(head,1);
+
+    deletion(head, 10); // safe invalid delete
     display(head);
-    
 
     return 0;
 }
